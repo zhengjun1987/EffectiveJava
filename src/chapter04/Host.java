f7830ef3507d6e63c99610569f9fa28561781613
Zheng Jun <zhengjun1987@outlook.com>
2018-02-24 11:22:54 +0800

    EffectiveJava chapter04 2018-2-24 11:22:51

--- src/chapter04/Host.java
+++ src/chapter04/Host.java
@@ -9,7 +9,14 @@ import java.util.Comparator;
  * Date: 2018/2/23 17:36
  */
 public class Host {
+    public Host() {
+        System.out.println("Host.<init>");
+    }
+
     private static class StrLenCmp implements Comparator<String>,Serializable {
+        public StrLenCmp() {
+            System.out.println("StrLenCmp.<init>");
+        }
 
         @Override
         public int compare(String o1, String o2) {
@@ -18,4 +25,12 @@ public class Host {
     }
 
     public static final Comparator<String> STRING_LENGTH_COMPARATOR = new StrLenCmp();
+
+    public static void main(String[] args) {
+        Host host = new Host();
+    }
 }
+//StrLenCmp.<init>
+//Host.<init>
+//
+//Process finished with exit code 0
--- src/chapter04/PhysicalConstants.java
+++ src/chapter04/PhysicalConstants.java
@@ -7,7 +7,7 @@ package chapter04;
  * 常量表达的反面典型:用接口存储常量,导致后续无法更改\删除该接口(public的东西就是一种承诺)
  */
 public interface PhysicalConstants {
-    static double AVOCADROS_NUMBER = 6.02214199e23;
+    static final double AVOCADROS_NUMBER = 6.02214199e23;
     static final double BOLTZMANN_CONSTANT = 1.3806503e-23;
     static final double ELECTRONIC_MASS = 9.10938188e-31;
 }
