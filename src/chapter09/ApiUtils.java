d458fd114f9e3cb93adb262cb0fb5cbc952705eb
Zheng Jun <zhengjun1987@outlook.com>
2018-03-08 17:16:52 +0800

    Effective Java chapter09 2018-3-8 17:16:45

--- src/chapter09/ApiUtils.java
+++ src/chapter09/ApiUtils.java
@@ -17,10 +17,26 @@ public class ApiUtils {
         return false;
     }
 
+
+    /**
+     * 检查字符串是否为空(包括字符串对象为null\""\"Null")
+     * 异常转译/异常链
+     * @author Zheng Jun
+     * @throws IllegalArgumentException 检查结果为空时抛出
+     */
     public static void checkNull(String argS) throws EmptyStringException {
         if (isEmpty(argS)) {
-            throw new EmptyStringException(1,"参数为空!");
+            throw new EmptyStringException(1, "参数为空!");
         }
+//        Exception in thread "main" java.lang.IllegalArgumentException
+//        at chapter09.ApiUtils.checkNull(ApiUtils.java:23)
+//        at chapter09.ExceptionTest.test(ExceptionTest.java:13)
+//        at chapter09.ExceptionTest.main(ExceptionTest.java:27)
+//        Caused by: chapter09.EmptyStringException: 参数为空!
+//                at chapter09.ApiUtils.checkNull(ApiUtils.java:22)
+//	    ... 2 more
+//
+//        Process finished with exit code 1
     }
 
     public static void checkNull(Object argS) throws IllegalArgumentException {
--- src/chapter09/ExceptionTest.java
+++ src/chapter09/ExceptionTest.java
@@ -9,17 +9,28 @@ import chapter02.MyUtils;
  * Date: 2018/3/7 15:41
  */
 public class ExceptionTest {
-    private static void test(String argS,int argI) throws UserException, EmptyStringException {
+    private static void test(String argS, int argI) throws UserException, EmptyStringException {
         ApiUtils.checkNull(argS);
-        if (argI==0) {
-            throw new UserException("第二个参数不得为零!");
+        if (argI == 0) {
+            UserException userException = new UserException("第二个参数不得为零!");
+            throw new IllegalArgumentException(userException);
         }
+        assert argI > 0 : "argI must be larger than 0";
         System.out.println(MyUtils.getCurrentTime() + "ExceptionTest.test  " + "argS = [" + argS + "], argI = [" + argI + "]");
 //        2018-03-07 16:01:08:960  ExceptionTest.test  argS = [TEST], argI = [2]
     }
 
     public static void main(String[] args) {
         String s = "NULL";
+//        Exception in thread "main" java.lang.IllegalArgumentException
+//        at chapter09.ApiUtils.checkNull(ApiUtils.java:23)
+//        at chapter09.ExceptionTest.test(ExceptionTest.java:13)
+//        at chapter09.ExceptionTest.main(ExceptionTest.java:27)
+//        Caused by: chapter09.EmptyStringException: 参数为空!
+//                at chapter09.ApiUtils.checkNull(ApiUtils.java:22)
+//	        ... 2 more
+//
+//        Process finished with exit code 1
         int i = 2;
         try {
             test(s, i);
